ab95bc06dd988264b4cd6d318ff9df410ecf686c
Juan Oaski Pegoretti <juan.opegoretti@outlook.com>
2021-02-24 08:56:34 -0300

    Countdown Button check-circle Icon

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -56,6 +56,7 @@ export function Countdown() {
                     className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
                     >
                     Ciclo encerrado
+                    <img src="icons/check-circle.svg" alt="Level" />
                 </button>
             ) : (
                     <>
